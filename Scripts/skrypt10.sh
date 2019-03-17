27904e31fd1abf1cbed558234520b91113081528
Tyberiusz <asmonix@gmail.com>
2019-03-17 21:38:14 +0100

    Hello Bash

---
+++ Scripts/skrypt10.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#Porównanie  zawartości  dwóch  zadanych  katalogów  (argumenty  skryptu).  Przy  porównaniu  
+#należy ignorować podkatalogi. W wyniku wyświetlić na ekranie listę plików o identycznych 
+#nazwach w obu katalogach. 
+
+echo Wypisanie plikow o identycznych nazwach w katologu: $1 i $2
+
+for fileA in `ls $1`; do
+	for fileB in `ls $2`; do
+		if [ -f "$1"/$fileA -a -f "$2"/$fileB -a $fileA = $fileB ]
+		then
+			echo $fileA jest w katalogu $1 i $2
+		fi
+	done 
+done
+
+#Komendy
+# $A = $B - jako sprawdzenie czy są takie same nazwy
---
+++ Scripts/skrypt14.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#Ustawienie  na  aktualny  czasu  ostatniej  modyfikacji  wszystkich  plików  zadanego  katalogu  
+#(parametr wywołania skryptu), do których mamy prawo dostępu do zapisu/modyfikacji. 
+
+echo Zmieniam czs ostatniej modyfikacji na aktualny w $1
+
+for file in $1/*;do
+	if [ -f $file ]
+	then
+		echo Zmieniam czas $file
+		touch -m $file
+	fi
+done 
+
+#Komendy
+# touch -m zmienia czas modyfikacji na aktualny
